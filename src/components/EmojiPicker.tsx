dfc743f6e61fdd93fc9f2fd15d0f07d1cb4728e9
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-07 14:46:46 +0000

    ran prettier

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,48 +1,46 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function EmojiPicker(): JSX.Element {
-    const [emojiFromCurrentRender, queueRerenderWithNewEmoji] =
-    useState("😊");
-    const [currentFavouriteEmoji, queueRerenderwithNewFavouriteEmoji] =
+  const [emojiFromCurrentRender, queueRerenderWithNewEmoji] = useState("😊");
+  const [currentFavouriteEmoji, queueRerenderwithNewFavouriteEmoji] =
     useState("");
 
-    const handleStoreCurrentEmoji = () => {
-        queueRerenderwithNewFavouriteEmoji(emojiFromCurrentRender);
-    };
+  const handleStoreCurrentEmoji = () => {
+    queueRerenderwithNewFavouriteEmoji(emojiFromCurrentRender);
+  };
 
-    const handleBigSmile = () => {
-        queueRerenderWithNewEmoji("😁");
-    };
-    const handleScared = () => {
-        queueRerenderWithNewEmoji("😨");
-    };
-    const handleInjured = () => {
-        queueRerenderWithNewEmoji("🤕");
-    };
-    const handleHeart = () => {
-        queueRerenderWithNewEmoji("❤️️");
-    };
-    const handleCool = () => {
-        queueRerenderWithNewEmoji("😎");
-    };
+  const handleBigSmile = () => {
+    queueRerenderWithNewEmoji("😁");
+  };
+  const handleScared = () => {
+    queueRerenderWithNewEmoji("😨");
+  };
+  const handleInjured = () => {
+    queueRerenderWithNewEmoji("🤕");
+  };
+  const handleHeart = () => {
+    queueRerenderWithNewEmoji("❤️️");
+  };
+  const handleCool = () => {
+    queueRerenderWithNewEmoji("😎");
+  };
 
-    return (
-        <>
-        <h1>FAVOURITE EMOJI PICKER</h1>
-        <p>Your Favourite Emoji: {currentFavouriteEmoji}</p>
-        <p>Your Current Emoji: {emojiFromCurrentRender}</p>
-        <button onClick={handleBigSmile}>😁</button>
-        &nbsp;
-        <button onClick={handleScared}>😨</button>
-        &nbsp;
-        <button onClick={handleInjured}>🤕</button>
-        &nbsp;
-        <button onClick={handleHeart}>❤️️</button>
-        &nbsp;
-        <button onClick={handleCool}>😎</button>
-
-        <hr />
-        <button onClick={handleStoreCurrentEmoji}>Make Favourite</button>
-        </>
-    )
+  return (
+    <>
+      <h1>FAVOURITE EMOJI PICKER</h1>
+      <p>Your Favourite Emoji: {currentFavouriteEmoji}</p>
+      <p>Your Current Emoji: {emojiFromCurrentRender}</p>
+      <button onClick={handleBigSmile}>😁</button>
+      &nbsp;
+      <button onClick={handleScared}>😨</button>
+      &nbsp;
+      <button onClick={handleInjured}>🤕</button>
+      &nbsp;
+      <button onClick={handleHeart}>❤️️</button>
+      &nbsp;
+      <button onClick={handleCool}>😎</button>
+      <hr />
+      <button onClick={handleStoreCurrentEmoji}>Make Favourite</button>
+    </>
+  );
 }
